8f53c0d9bb9f36babf5223dc43b398499d36d05f
KemonoBAT4 <kemono.bat.4@gmail.com>
2025-11-26 00:49:11 +0100

    [cardinal] started to implement a arg based structure

--- run.py
+++ run.py
@@ -1,43 +1,17 @@
 from core.system import cardinal
+import sys
+
+possible_args = ["setup", ]
+
 
 if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        arg = sys.argv.pop()
+        print(arg)
+        if arg == "setup":
+            print("Setting up database...")
+            cardinal.setup()
+        #endif
+    #endif
     cardinal.run()
 #endif
-
-
-
-# import os
-# import importlib
-# from flask import Flask
-
-# app = Flask(__name__)
-
-# def addBlueprint(bp, prefix):
-#     print("addBlueprint called")
-#     app.register_blueprint(bp, url_prefix=prefix)
-# #enddef
-
-# @app.route('/')
-# def hello():
-#     return 'Hello, World!'
-# #enddef
-
-# # Dynamically import all route files in the 'routes' directory
-# app_dir = 'app'
-# for folder in os.listdir(app_dir):
-#     folder_path = os.path.join(app_dir, folder)
-#     if os.path.isdir(folder_path):
-#         routes_file = os.path.join(folder_path, 'routes.py')
-#         if os.path.isfile(routes_file):
-#             module_name = os.path.splitext(os.path.basename(routes_file))[0]
-#             module = importlib.import_module(f'{app_dir}.{folder}.{module_name}')
-#             bp = getattr(module, module_name)
-#             addBlueprint(bp, f'/{folder}')
-#         #endif
-#     #endif
-# #endfor
-
-# if __name__ == '__main__':
-#     app.run()
-# #endif
-
