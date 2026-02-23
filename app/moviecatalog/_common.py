864e395cb4f4bf7ec6388e1c4049d2ecec18608d
KemonoBAT4 <kemono.bat.4@gmail.com>
2026-02-23 05:16:34 +0100

    [app] made updates to the _common file, added a placeholder to the templates folder for future developments

--- app/moviecatalog/_common.py
+++ app/moviecatalog/_common.py
@@ -2,17 +2,17 @@
 # flask imports
 from flask import Blueprint, redirect, url_for
 from flask import render_template, send_from_directory
+from flask_login import login_required
 
 # core imports
 from core.models import *
 from core.configs import *
 from core.web import *
+from core.mail import *
 
 # other imports
 import os
 import typing
-import types
 from enum import Enum
 
 project_name: str = os.path.dirname(os.path.abspath(__file__))
-
