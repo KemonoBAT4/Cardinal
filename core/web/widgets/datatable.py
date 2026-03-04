b8f3f13cb72ddbac8bb71b9e118d28175fe8d07e
KemonoBAT4 <kemono.bat.4@gmail.com>
2026-03-04 03:25:09 +0100

    [core] created new widget and updated the datatable widget with comments

--- core/web/widgets/datatable.py
+++ core/web/widgets/datatable.py
@@ -1,12 +1,24 @@
 
+from ._common import *
 
+
+# NOTE: this class is not yet implemented
+# TODO: implement this class
+# the class should create a table with the given data (url or dataframe, need
+# to decide which is better). then the class should render the table with all
+# the actions possible (click event on the table rows, pdf download,
+# excel download, etc.).a searchbar should be added too for better
+# searching in the table in case of large quantities of records
 class CardinalDataTable():
 
-    def __init__(self):
+    def __init__(
+        self
+    ) -> "None":
         pass
     # #enddef
 
-    def render(self):
-        pass
+    def render(self) -> "str":
+        """Renders the data table"""
+        return ""
     # #enddef
 # #endclass CardinalDataTable
---
+++ core/web/widgets/grid.py
@@ -0,0 +1,19 @@
+
+from ._common import *
+
+# NOTE: this class is not yet implemented
+# TODO: implement this class
+# the class should create a grid of items
+class CardinalGrid():
+
+    def __init__(
+        self
+    ) -> "None":
+        pass
+    # #enddef
+
+    def render(self) -> "str":
+        """Renders a grid of items"""
+        return ""
+    # #enddef
+# #endclass CardinalGrid
