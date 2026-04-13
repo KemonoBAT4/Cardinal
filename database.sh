89f985c72ccfa565fb32833de034d9203482e2fe
KemonoBAT4 <kemono.bat.4@gmail.com>
2026-04-13 23:27:02 +0200

    [cardinal] added scripts to perform multiple actions for every specific project

---
+++ database.sh
@@ -0,0 +1,16 @@
+# TODO: implement this script
+
+# NOTE: when using this script, it can interact with the database
+# the name of the wanted application must be passed as the first argument
+
+# NOTE: if the second argument is --reset or similar (a reset command)
+# then the script has to completely reset the database for that application ONLY
+
+# NOTE: if the second argument is --migrate or similar (a migrate command)
+# then the script has to migrate the database for that application ONLY
+
+# NOTE: if the second argument is --shell or similar (a seed command)
+# then the script has enter the database's shell so the user can perform commands there
+
+# NOTE: any other argument will be passed or converted based on what 
+# the application needs to execute this script
---
+++ run.sh
@@ -0,0 +1,9 @@
+# TODO: implement this script
+
+# NOTE: when using this script, it can execute any application in the "app" folder
+# the name of the wanted application must be passed as the first argument
+
+# NOTE: if the application wants to be started as setup (so by executing the "setup" function in the designated application)
+# then the second argument must be "setup"
+
+# NOTE: any other argument will be passed or converted based on what it is or what the application needs to run
